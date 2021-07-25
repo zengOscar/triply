3463d141fa1e91827bf1cfa1f03719c9ab62f404
zengOscar <oscarczeng@gmail.com>
2021-07-25 01:59:37 -0500

    Home screen

---
+++ ui/src/components/pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '',
+    headline: 'Welcome',
+    description: 'What would you like to do today?',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Credit Card'
+}
